3e055afa86c308c4b8d85763755ef4bb74e3199c
Angel Acatitla <angelgonzalez477@gmail.com>
2020-10-16 00:23:05 -0500

    estan listas las cartas

---
+++ ejercicios/videos.js
@@ -0,0 +1,24 @@
+const titulo = document.getElementById('titulo');
+const video = document.getElementById('video');
+const generarPedido = () => {
+    const url = new URL(window.location.href);
+    const categoria = url.searchParams.get('categoria');
+    if (categoria == '' || categoria == undefined) {
+        alert('categoria inválido, vuelve a intentarlo');
+        window.location.href = 'index.html';
+    } else {
+        if (categoria === 'abecedario') {
+            titulo.innerHTML = 'Abecedario';
+            video.src = '';
+        } else if (categoria === 'espa') {
+            titulo.innerHTML = 'Español';
+            video.src = '';
+        } else if (categoria === 'mate') {
+            titulo.innerHTML = 'Matemáticas';
+            video.src = '';
+        } else if (categoria === 'histo') {
+            titulo.innerHTML = 'Historia';
+            video.src = '';
+        }
+    }
+}
